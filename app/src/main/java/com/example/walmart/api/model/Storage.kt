bb3a5b74d434a36bbcdcaa6be0f50c3832c0ab25
CodeAsura <arturojavier@live.com>
2024-12-16 11:40:31 -0600

    Delete app directory

--- app/src/main/java/com/example/walmart/api/model/Storage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.walmart.api.model
-
-data class Storage(
-    val id: Int,
-    val name: String,
-    val address: String,
-    val latitude: String,
-    val longitude: String
-)
--- app/src/main/java/com/example/walmart/api/service/ProductService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.walmart.api.service
-
-import com.example.walmart.api.model.Product
-import retrofit2.http.GET
-
-interface ProductService {
-    @GET("/api/v1/product")
-    suspend fun getProducts(): List<Product>
-}
